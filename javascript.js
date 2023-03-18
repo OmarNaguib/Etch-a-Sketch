dd274daeeba7b776f6b40e5e102319cb0819a663
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-18 06:50:14 +0200

    add newGrid button

--- javascript.js
+++ javascript.js
@@ -1,14 +1,31 @@
-let length = 16;
-let container=document.querySelector("#container")
 
-for (let i = 1;i<=16;i++) {
-    let row =document.createElement("div")
-    row.classList.add("row")
-    for (let i = 1;i<=16;i++) {
-        let square = document.createElement("div");
-        square.classList.add("square")
-        row.appendChild(square)
+function createGrid(length=16) {
+    let container = document.createElement("div")
+    container.id="container"
+    document.body.appendChild(container)
 
+    for (let i = 1;i<=length;i++) {
+        let row =document.createElement("div")
+        row.classList.add("row")
+        for (let i = 1;i<=16;i++) {
+            let square = document.createElement("div");
+            square.addEventListener("mouseover",() => square.classList.toggle("hover"))
+            square.classList.add("square")
+            row.appendChild(square)
+
+        }
+        container.appendChild(row)
     }
-    container.appendChild(row)
 }
+
+function changeGrid() {
+    let length = Math.min(100,prompt("Choose the new size for your grid"))
+    let container = document.querySelector("#container")
+    container.remove()
+    createGrid(length)
+}
+
+let newGridButton = document.querySelector("#newGrid")
+newGridButton.addEventListener("click",changeGrid)
+
+createGrid();
