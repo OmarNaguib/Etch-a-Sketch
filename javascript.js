52fcd814fd5b17b09537e3615c69af33ad5e0ff9
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-03-20 03:04:02 +0200

    add 10% black colorMethod

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ function createGrid(length=16) {
     for (let i = 1;i<=length;i++) {
         let row =document.createElement("div")
         row.classList.add("row")
-        for (let i = 1;i<=16;i++) {
+        for (let i = 1;i<=length;i++) {
             let square = document.createElement("div");
             square.addEventListener("mouseover",() => colorSquare(square))
             square.classList.add("square")
@@ -27,8 +27,18 @@ function changeGrid() {
 
 function colorSquare(square) {
     if (colorMethod === "default") {square.classList.toggle("hover")}
-    if (colorMethod === "random")  {square.style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16);}
-    
+    else if (colorMethod === "random")  {square.style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16);}
+    else {darken(square)}
+}
+
+function darken(square) {
+    let color= window.getComputedStyle(square).backgroundColor
+    let colorList=color.slice(4,color.length-1).split(",")
+    let colorListDarkend = colorList.map((color)=> Math.max(0,Math.floor(color-25.5)))
+    color = "rgb(" + colorListDarkend.join(",") + ")"
+    square.style.backgroundColor = color
+
+
 }
 
 let newGridButton = document.querySelector("#newGrid")
